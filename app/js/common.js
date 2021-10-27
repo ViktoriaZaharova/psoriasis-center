f720782ee3b79174777b19ade4469057efd10fcc
Viktoria <zaharova181114@gmail.com>
2021-10-27 14:59:11 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,19 @@
+$('.go_to').click(function (e) {
+    e.preventDefault();
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length !== 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    $('.mobile-menu').fadeOut();
+
+    return false;
+});
+
+$('.certificates-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
