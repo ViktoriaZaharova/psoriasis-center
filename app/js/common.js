de31145996311d877255ec57892160e9eb3dfc53
Viktoria <zaharova181114@gmail.com>
2021-10-28 12:12:57 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -21,7 +21,15 @@ $('.certificates-slider').slick({
 $('.reviews-slider').slick({
     slidesToShow: 2,
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
 $('.result-slider').slick({
@@ -30,3 +38,100 @@ $('.result-slider').slick({
     dots: true,
     fade: true
 });
+
+$('[name="phone"]').mask('+7 999 999 99-99');
+
+// mobile menu
+$('.btn-burger').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeOut();
+});
+
+
+
+// модальные окна (несколько)
+$(function () {
+    let overlay = $('.overlay'),
+        open_modal = $('.open_modal'),
+        close = $('.modal__close, .overlay'),
+        modal = $('.modal__div');
+
+    open_modal.on('click',function (event) {
+        event.preventDefault();
+
+        modal.css('display', 'none').animate({
+            opacity: 0,
+            top: '45%'
+        }, 200);
+
+        let div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.on('click', function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
+
+// отправка заявки с формы обратной связи на почту
+$(document).ready(function () {
+
+    $(".form").submit(function () {
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            // alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+
+            // если после отправки нужно показать окно "спасибо" и скрыть его через секунду
+            $('.modal__div').css('display', 'none').animate({
+                opacity: 0,
+                top: '45%'
+            });
+
+            $('#thanks__modal').css('display', 'flex')
+                .animate({
+                    opacity: 1,
+                    top: '50%'
+                }, 200);
+
+            setTimeout(function () {
+                $("#thanks__modal").css('display', 'none').animate({
+                    opacity: 0,
+                    top: '45%'
+                });
+                $('.overlay').fadeOut(400);
+            }, 3000);
+            // если после отправки нужно показать окно "спасибо" и скрыть его через секунду
+            $(".form").trigger("reset");
+        });
+        return false;
+    });
+
+});
+// end
---
+++ app/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "test@gmail.com";
+$sitename = "Альфа фасад-центр";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$mail = trim($_POST["mail"]);
+$message = trim($_POST["message"]);
+$message = "Имя: $name \nТелефон: $phone \nEmail: $mail \nСообщение: $message";
+
+$pagetitle = "Заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
